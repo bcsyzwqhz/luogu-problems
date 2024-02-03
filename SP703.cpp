e5384a8fa4fe92178c2ff7ddedfac8406a7f6c82
bcsyzwqhz <makerlai@163.com>
2024-02-03 20:04:24 +0800

    AC!

--- SP703.cpp
+++ SP703.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int c[201][201];
 int f[1001][201][201];
-int t[1001];
+int p[1001];
 int main()
 {
     ios::sync_with_stdio(0);
@@ -14,24 +14,22 @@ int main()
     {
         cin>>l>>n;
         for(int i=1;i<=l;i++)
-            for(int j=1;i<=l;j++)
+            for(int j=1;j<=l;j++)
                 cin>>c[i][j];
         for(int i=1;i<=n;i++)
-            cin>>t[i];
-        memset(f,63,sizeof(f));
+            cin>>p[i];
+        memset(f,0X3F,sizeof(f));
         f[0][1][2]=0;
-        t[0]=3;
-        for(int i=0;i<n;i++)
-            for(int j=1;i<=l;j++)
-                for(int k=1;k<=l;k++)
-                {
-                    if(k==j||i==t[i]||j==t[i])  continue;
-                    if(t[i]!=t[i+1]&&k!=t[i+1])
-                        f[i+1][t[i]][k]=min(f[i+1][t[i]][k],f[i][j][k]+c[j][t[i+1]]);
-                    if(t[i]!=t[i+1]&&j!=t[i+1])
-                        f[i+1][j][t[i]]=min(f[i+1][j][t[i]],f[i][j][k]+c[k][t[i+1]]);
-                    if(j!=t[i+1]&&k!=t[i+1])
-                        f[i+1][j][k]=min(f[i+1][j][k],f[i][j][k]+c[t[i]][t[i+1]]);
+        p[0]=3;
+        for (int i=0;i<n;i++)
+            for (int x=1;x<=l;x++)
+                for (int y=1;y<=l;y++)
+				{
+                    if(x==p[i]||x==y||y==p[i])
+                        continue;
+                    f[i+1][p[i]][y]=min(f[i+1][p[i]][y],f[i][x][y]+c[x][p[i+1]]);
+                    f[i+1][x][p[i]]=min(f[i+1][x][p[i]],f[i][x][y]+c[y][p[i+1]]);
+                    f[i+1][x][y]=min(f[i+1][x][y],f[i][x][y]+c[p[i]][p[i+1]]);
                 }
         ans=0x3f3f3f3f;
         for(int i=1;i<=l;i++)
